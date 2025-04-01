5b2cae958e6428f3697a872940a637cc2d86699f
nsulonen <niko.sulonen@proton.me>
2025-04-01 12:19:28 +0300

    define methods

---
+++ main.py
@@ -0,0 +1,8 @@
+from .utils.converters import *
+
+def main() -> None:
+    raise NotImplementedError("main function not implemented")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/converters.py
@@ -0,0 +1,11 @@
+def binary_to_decimal() -> None:
+    raise NotImplementedError("binary_to_decimal not implemented")
+
+def decimal_to_binary() -> None:
+    raise NotImplementedError("decimal_to_binary not implemented")
+
+def binary_to_hexadecimal() -> None:
+    raise NotImplementedError("binary_to_hexadecimal not implemented")
+
+def hexadecimal_to_binary() -> None:
+    raise NotImplementedError("hexadecimal_to_binary not implemented")
