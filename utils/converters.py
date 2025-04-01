454905758997a89f833a4cfe03878bef6782d443
nsulonen <niko.sulonen@proton.me>
2025-04-01 12:25:47 +0300

    define more methods

--- utils/converters.py
+++ utils/converters.py
@@ -4,6 +4,12 @@ def binary_to_decimal() -> None:
 def decimal_to_binary() -> None:
     raise NotImplementedError("decimal_to_binary not implemented")
 
+def hexadecimal_to_decimal() -> None:
+    raise NotImplementedError("hexadecimal_to_decimal not implemented")
+
+def decimal_to_hexadecimal() -> None:
+    raise NotImplementedError("decimal_to_hexadecimal not implemented")
+
 def binary_to_hexadecimal() -> None:
     raise NotImplementedError("binary_to_hexadecimal not implemented")
 
